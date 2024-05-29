256d4c6846a25c326076891db73864b29a08a0b2
aliahsan676 <aliahsan676@gmail.com>
2024-05-29 22:24:06 +0600

    "first commit"

---
+++ src/test/java/com/ui/test/UITest.java
@@ -0,0 +1,23 @@
+package com.ui.test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
+public class UITest {
+    public static void main(String[] args) {
+
+        //Browser Launch
+
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+
+        driver.get("https://www.amazon.com");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+
+    }
+}
